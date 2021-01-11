50b3767e94e19551cce7c5010ee4c1c34de6f84c
seeebiii <info@sebastianhesse.de>
2021-01-11 20:10:55 +0100

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface ServerlessSesEmailsProps {
+  // Define construct properties here
+}
+
+export class ServerlessSesEmails extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: ServerlessSesEmailsProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
